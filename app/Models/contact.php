f8e9056e183bf4da668df33c393b0af3936468f5
Houssam OUATMMANI <ouatmanihoussam@gmail.com>
2023-08-01 20:55:30 +0200

    Add Role to users table, relation between categories and messages

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
@@ -11,4 +13,9 @@ class Contact extends Model
     protected $fillable = [
         'firstname', 'lastname', 'email', 'phone', 'category_id', 'message'
     ];
+
+
+    public function category() : BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+Route::get('/admin', [AdminController::class, 'index']);
